14c3dc3450b2058a91ec61b6d91ec1f3fbfb2886
Mehmet <mehmetk6158@gmail.com>
2022-01-18 21:56:39 -0500

    add footer

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,11 @@
     <!-- </nav> -->
     <!-- Page Content-->
     <router-view />
-    <!-- Footer-->
+    <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+      <div class="container text-center">
+        <small>Copyright &copy; WalkeronRails</small>
+      </div>
+    </footer>
   </div>
 </template>
 
